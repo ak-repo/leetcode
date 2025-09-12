0502df90012812ae9a783d343ec57ae13780874e
Ananda krishnan <202474683+ak-repo@users.noreply.github.com>
2025-09-12 10:58:08 +0530

    Time: 87 ms (5.16%) | Memory: 7.7 MB (97.58%) - LeetSync

---
+++ 268-missing-number/missing-number.go
@@ -0,0 +1,16 @@
+func missingNumber(nums []int) int {
+
+    n:= len(nums)
+
+    for i:=0;i<=n;i++{
+        if !slices.Contains(nums,i){
+            return i
+        }
+    }
+    return -1
+
+
+  
+    
+    
+}
