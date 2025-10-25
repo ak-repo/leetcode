b5cefa7d21876dac7691bde1b8d91f737793d3bd
Ananda krishnan <202474683+ak-repo@users.noreply.github.com>
2025-10-25 16:13:48 +0530

    Time: 0 ms (100%), Space: 4.5 MB (58.09%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.go
@@ -0,0 +1,13 @@
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    current := head
+
+    for current != nil {
+        next := current.Next  
+        current.Next = prev   
+        prev = current        
+        current = next       
+    }
+
+    return prev  
+}
