127ef98837b2a2b99200d28ab60457cfb31b81a9
Ananda krishnan <202474683+ak-repo@users.noreply.github.com>
2025-10-27 19:40:30 +0530

    Time: 0 ms (100%), Space: 4.2 MB (12.11%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.go
@@ -0,0 +1,15 @@
+func sortColors(nums []int)  {
+
+
+
+	for i := 0; i < len(nums)-1; i++ {
+		minIndex := i
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < nums[minIndex] {
+				minIndex = j
+			}
+		}
+		nums[i], nums[minIndex] = nums[minIndex], nums[i]
+	}
+    
+}
