a5f54fa5a7b16f918832c6a3cd90ce5be789d0af
Ananda krishnan <202474683+ak-repo@users.noreply.github.com>
2025-11-06 15:25:51 +0530

    Time: 154 ms (11.94%), Space: 44.1 MB (5.07%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
@@ -1,8 +1,28 @@
 func findKthLargest(nums []int, k int) int {
-	sort.Ints(nums)
 
-	return nums[len(nums)-k]
-    
+arr:= QuickSort(nums)
+
+return arr[k-1]
 }
 
 
+func QuickSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	pivot := arr[len(arr)/2]
+	var less, equal, greater []int
+
+	for _, v := range arr {
+		if v < pivot {
+			less = append(less, v)
+		} else if v > pivot {
+			greater = append(greater, v)
+		} else {
+			equal = append(equal, v)
+		}
+	}
+
+	return append(append(QuickSort(greater), equal...), QuickSort(less)...)
+}
